66fa9675a2d0e20db67b8b5362184a0b6efeb085
Josiah Broughton <hohaia.code@gmail.com>
2024-03-17 22:16:16 +1300

    ingredient/recipte type tables implemented in database
    The most significant changes involve the addition of new interfaces and classes to handle operations related to `IngredientType` and `RecipeType` entities. These changes indicate a shift from storing data in a text file to a database. The `IngredientsTxtRepository` class, which was previously used for handling `Ingredient` objects, has been removed.
    
    1. Addition of `IIngredientTypesRepository` and `IRecipeTypesRepository` interfaces: These interfaces define the contract for the repositories that will handle operations related to `IngredientType` and `RecipeType` respectively. They both have an `OnError` event, `Add` and `Get` methods.
    
    2. Addition of `IngredientTypesRepository` and `RecipeTypesRepository` classes: These classes implement the `IIngredientTypesRepository` and `IRecipeTypesRepository` interfaces respectively. They both have methods to add and retrieve data from the database, and they handle SQL exceptions and other exceptions by logging the error and invoking the `OnError` event.
    
    3. Removal of `IngredientsTxtRepository` class: This class was previously used to handle operations related to `Ingredient` objects, but it seems like the application is transitioning from storing data in a text file to storing data in a database.
    
    4. Addition of `IngredientType` and `RecipeType` classes: These classes represent the `IngredientType` and `RecipeType` entities in the domain model. They both have `Id` and `Name` properties.

---
+++ DataAccessLayer/Repositories/IngredientTypesRepository.cs
@@ -0,0 +1,73 @@
+﻿using DataAccessLayer.Contracts;
+using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Logging;
+using Dapper;
+
+namespace DataAccessLayer.Repositories
+{
+    public class IngredientTypesRepository : IIngredientTypesRepository
+    {
+        public event Action<string> OnError;
+
+        private void ErrorOccured(string errorMessage, Exception ex)
+        {
+            if (OnError != null)
+                OnError.Invoke(errorMessage);
+
+            Logger.LogError(DateTime.Now.ToString(), ex.Message);
+        }
+
+        public async Task AddIngredientType(IngredientType ingredientType)
+        {
+            try
+            {
+                string query = @"insert into IngredientTypes (Name) values (@Name)";
+
+                using (IDbConnection connection = new SqlConnection(ConnectionHelper.ConnectionString))
+                {
+                    await connection.ExecuteAsync(query, ingredientType);
+                }
+            }
+            catch (SqlException ex)
+            {
+                string errorMessage = "";
+                if (ex.Number == 2627)
+                    errorMessage = "That ingredient type already exists in the database!";
+                else
+                    errorMessage = "An error occurred in the database!";
+                ErrorOccured(errorMessage, ex);
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = "An error occurred while adding an ingredient type!";
+                ErrorOccured(errorMessage, ex);
+            }
+        }
+
+        public async Task<List<IngredientType>> GetIngredientTypes()
+        {
+            try
+            {
+                string query = "select * from IngredientTypes order by Name asc";
+
+                using (IDbConnection connection = new SqlConnection(ConnectionHelper.ConnectionString))
+                {
+                    return (await connection.QueryAsync<IngredientType>(query)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = "An error occurred while retieving the ingredient types!";
+                ErrorOccured(errorMessage, ex);
+                return new List<IngredientType>();
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/RecipeTypesRepository.cs
@@ -0,0 +1,74 @@
+﻿using DataAccessLayer.Contracts;
+using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Utility.Logging;
+using System.Globalization;
+
+namespace DataAccessLayer.Repositories
+{
+    public class RecipeTypesRepository : IRecipeTypesRepository
+    {
+        public event Action<string> OnError;
+
+        private void ErrorOccured(string errorMessage, Exception ex)
+        {
+            if (OnError != null)
+                OnError.Invoke(errorMessage);
+
+            Logger.LogError(DateTime.Now.ToString(), ex.Message);
+        }
+
+        public async Task AddRecipeType(RecipeType recipeType)
+        {
+            try
+            {
+                string query = @"insert into RecipeTypes (Name) values (@Name)";
+
+                using (IDbConnection connection = new SqlConnection(ConnectionHelper.ConnectionString))
+                {
+                    await connection.ExecuteAsync(query, recipeType);
+                }
+            }
+            catch (SqlException ex)
+            {
+                string errorMessage = "";
+                if (ex.Number == 2627)
+                    errorMessage = "That recipe type already exists in the database!";
+                else
+                    errorMessage = "An error occurred in the database!";
+                ErrorOccured(errorMessage, ex);
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = "An error occurred while adding a recipe type!";
+                ErrorOccured(errorMessage, ex);
+            }
+        }
+
+        public async Task<List<RecipeType>> GetRecipeTypes()
+        {
+            try
+            {
+                string query = "select * from RecipeTypes order by Name asc";
+
+                using (IDbConnection connection = new SqlConnection(ConnectionHelper.ConnectionString))
+                {
+                    return (await connection.QueryAsync<RecipeType>(query)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = "An error occurred while retieving the recipe types!";
+                ErrorOccured(errorMessage, ex);
+                return new List<RecipeType>();
+            }
+        }
+    }
+}
