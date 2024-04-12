a6bbfef784bd21ef430d77db0dffe4852e3dba88
Josiah Broughton <hohaia.code@gmail.com>
2024-04-13 11:35:34 +1200

    DB rebuilt
    The most significant changes include the update of the Dapper package in DataAccessLayer.csproj, the modification of the server name and password in the connection string in App.config, and the removal of a line in IngredientsForm.Designer.cs.
    
    1. The Dapper package in DataAccessLayer.csproj was updated from version 2.1.28 to 2.1.35. This update could potentially improve the performance and stability of the data access layer in the application. (Reference: DataAccessLayer.csproj)
    
    2. The server name in the connection string in App.config was changed to include "\SQLEXPRESS". This change indicates that the application will now connect to a SQL Server Express database. (Reference: App.config)
    
    3. The password in the connection string in App.config was updated. This change is crucial for maintaining the security of the database connection. (Reference: App.config)
    
    4. A commented out line in the appSettings section of App.config that added a key-value pair for "repositoryType" was removed. This change might not have a direct impact on the application since the line was previously commented out. (Reference: App.config)

--- Kai/UI/IngredientsForm.Designer.cs
+++ Kai/UI/IngredientsForm.Designer.cs
@@ -181,7 +181,6 @@
             // 
             TypeDrop.DropDownStyle = ComboBoxStyle.DropDownList;
             TypeDrop.FormattingEnabled = true;
-            TypeDrop.Items.AddRange(new object[] { "Condiment", "Dairy", "Fruit", "Ingredient", "Liquid", "Meat", "Nut", "Spice", "Sweetener", "Vegetable" });
             TypeDrop.Location = new Point(908, 120);
             TypeDrop.Name = "TypeDrop";
             TypeDrop.Size = new Size(193, 33);
